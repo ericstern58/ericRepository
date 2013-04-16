1f5e02260d4ea480ffe58766add5b7dac718c534
ericstern58 <ericstern58@yahoo.com>
2013-04-16 01:45:55 -0700

    Update flood.js

--- flood.js
+++ flood.js
@@ -23,9 +23,7 @@ drawApp.canvas.off('mousedown')\n\
 drawApp.canvas.on('mousedown',function(e){\n\
 	if($('#brush-fill').hasClass('selected')){\n\
         floodFill(e)\n\
-	} else if($('#brush-img').hasClass('selected')) { \n\
-		imgTest()\n\
-	}else{\n\
+	} else{\n\
 		md(e)\n\
 	}\n\
 })\n\
