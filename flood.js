9142b1dea18da08bb951c3a77bd5eea8cbc12655
ericstern58 <ericstern58@yahoo.com>
2013-04-16 22:10:21 -0700

    Update flood.js

--- flood.js
+++ flood.js
@@ -56,7 +56,9 @@ function floodFill(e){\n\
 	function f(xinitial,yinitial){\n\
 		var queue = []\n\
 		queue.push(new Point(xinitial,yinitial))\n\
-		for(var point, var x, var y; queue.length>0; point=queue.shift()) {\n\
+		var x = 0\n\
+		var y = 0\n\
+		for(var point; queue.length>0; point=queue.shift()) {\n\
 			x=point.x\n\
 			y=point.y\n\
 			if(x>=0 && y>=0 && x<w && y<h && targetColor.equals(new RGBColor(d[4*w*y+4*x],d[4*w*y+4*x+1],d[4*w*y+4*x+2])) ) {\n\
