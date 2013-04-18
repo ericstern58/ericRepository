87c154ce6e5ba3d88564382f3f376004e072b560
ericstern58 <ericstern58@yahoo.com>
2013-04-18 01:45:39 -0700

    Update flood.js

--- flood.js
+++ flood.js
@@ -42,7 +42,7 @@ function virtualLine(e){\n\
 	canvas.addEventListener('mousemove', update, false)\n\
 	canvas.addEventListener('mouseup', function() {\n\
 		var pos = getElementAbsolutePos(canvas)\n\
-		alert('start(' + start.x + ',' + start.y + ') end()' + ' pos('+pos.x+','+pos.y+')')\n\
+		alert('start(' + start.x + ',' + start.y + ') end('+(e.pageX-pos.x)+','+(e.pageY-pos.y)+')' + ' pos('+pos.x+','+pos.y+')')\n\
 		//'+e.pageX+','+e.pageY+')'+'offsetleft'+canvas.offsetLeft+' offsetTop'+canvas.offsetTop\n\
 		makeLine(start,new Point(e.pageX-pos.x,e.pageY-pos.y))\n\
 		canvas.removeEventListener('mousemove', update, false)\n\
