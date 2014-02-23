ad3c46631ec48aadeaf69c9b0537cda3aca0a132
ericstern58 <ericstern58@yahoo.com>
2014-02-22 21:24:02 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -1,1 +1,17 @@
 document.body.style.backgroundColor = "#AA4930";
+createButton("blue")
+
+function changeblue(){
+	document.body.style.backgroundColor = "#0713dd";
+}
+
+
+function createButton(name){
+	var button=document.createElement('a');
+	document.getElementById('buttonSpace').parentNode.appendChild(button);
+	button.id='brush-' + name;
+	button.className='brushPicker';
+	button.innerHTML=name;
+	button.onclick=function(){changeblue()};
+	return button;
+}
