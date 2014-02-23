49308ce2b0c2107454157c7db2b7e72aaa1a2d33
ericstern58 <ericstern58@yahoo.com>
2014-02-22 21:07:27 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -1,6 +1,1 @@
-var code="window.resizeTo(1280, 720);\n\
-"
-document.body.style.background = red;
-var js=document.createElement("script")
-js.text=code
-document.body.appendChild(js)
+(function(){document.body.style.background = red;})();
