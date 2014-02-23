7227a930405262427d9398c485807eb13e5ef299
ericstern58 <ericstern58@yahoo.com>
2014-02-22 22:17:43 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -5,13 +5,93 @@ drawToolsDiv.className = 'btn-group';
 document.getElementById('redo-button').parentNode.parentNode.appendChild(drawToolsDiv);
 
 //Create Tool Buttons
-createTool("Blu");
-createTool("Red");
-createTool("Fill");
-createTool("Line");
-createTool("Poly");
+var fillButton = createToolButton("Fill");
+var lineButton = createToolButton("Line");
+var polygonButton = createToolButton("Poly");
+var fillButton = createToolButton("Line");
 
-function createTool(name){
+
+/*--------------------- Button Methods ----------------------*/
+function floodFill(e){
+	save();
+	var w=drawApp.canvas.width();
+	var h=drawApp.canvas.height();
+	var p=drawApp.context.getImageData(0,0,w,h);
+	var d=p.data;
+	var targetColor = new RGBColor(d[4*w*e.offsetY+4*e.offsetX],d[4*w*e.offsetY+4*e.offsetX+1],d[4*w*e.offsetY+4*e.offsetX+2]);
+	var c=parseInt(drawApp.context.strokeStyle.substr(1,6),16);
+	var fillColor = new RGBColor((c>>16)&255,(c>>8)&255,c&255);
+	
+	try{
+		if(!targetColor.equals(fillColor))
+			f(e.offsetX,e.offsetY);
+	} catch(err) {
+		alert(err);
+	}
+	context.putImageData(p,0,0);
+	function f(xinitial,yinitial){
+		var queue = [new Point(xinitial,yinitial)];
+		var x = 0;
+		var y = 0;
+		while(queue.length>0) {
+			var point=queue.shift();
+			x=point.x;
+			y=point.y;
+			if(x>=0 && y>=0 && x<w && y<h && targetColor.equals(new RGBColor(d[4*w*y+4*x],d[4*w*y+4*x+1],d[4*w*y+4*x+2])) ) {
+				colorPixel(d,w,point,fillColor)
+				queue.push(new Point(x-1,y));
+				queue.push(new Point(x+1,y));
+				queue.push(new Point(x,y-1));
+				queue.push(new Point(x,y+1));
+			} else if(x>=0 && y>=0 && x<w && y<h){
+				//colorPixel(d,w,point,fillColor)
+				/*
+				colorPixelBlend(d,w,x-1,y,fillColor,new RGBColor(d[4*w*y+4*(x-1)],d[4*w*y+4*(x-1)+1],d[4*w*y+4*(x-1)+2]));
+				colorPixelBlend(d,w,x+1,y,fillColor,new RGBColor(d[4*w*y+4*(x+1)],d[4*w*y+4*(x+1)+1],d[4*w*y+4*(x+1)+2]));
+				colorPixelBlend(d,w,x,y-1,fillColor,new RGBColor(d[4*w*(y-1)+4*x],d[4*w*(y-1)+4*x+1],d[4*w*(y-1)+4*x+2]));
+				colorPixelBlend(d,w,x,y+1,fillColor,new RGBColor(d[4*w*(y+1)+4*x],d[4*w*(y+1)+4*x+1],d[4*w*(y+1)+4*x+2]));
+				*/
+			}
+		}
+	}
+}
+
+
+/*---------------------- Color Methods ----------------------*/
+//Colors a pixel with a given color
+function colorPixel(d,w,point,color){\n\
+	d[4*w*point.y+4*point.x]=color.r;\n\
+	d[4*w*point.y+4*point.x+1]=color.g;\n\
+	d[4*w*point.y+4*point.x+2]=color.b;\n\
+	d[4*w+point.y+4*point.x+3]=255;\n\
+}\n\
+//Colors a pixel with a blend of 2 colors (helpful for assimilating anti-aliasing)
+function colorPixelBlend(d,w,point,color1,color2){\n\
+	var r=Math.ceil(0.5*color1.r + 0.5*r2)\n\
+	var g=Math.ceil(0.5*color1.g + 0.5*g2)\n\
+	var b=Math.ceil(0.5*color1.b + 0.5*b2)\n\
+	colorPixel(d,w,point,new Color(r,g,b))\n\
+}\n\
+
+/*---------------------- Custom Objects ----------------------*/
+//Point Object
+function Point(x,y) {
+	this.x=x;
+	this.y=y;
+}
+//Color Object
+function RGBColor(r,g,b) {
+	this.r=r;
+	this.g=g;
+	this.b=b;
+	this.equals = function(color) {
+		return (this.r===color.r && this.g===color.g && this.b===color.b);
+	}
+}
+
+/*--------------------- Element Creation ---------------------*/
+//Creates Tool Buttons
+function createToolButton(name){
 	var button=document.createElement('a');
 	button.id='brush-' + name;
 	button.className='btn btn-yellow btn-drawtool';
