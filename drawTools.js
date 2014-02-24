247f8f2f43b3e0628c48a3d94b8099fb37ea947c
ericstern58 <ericstern58@yahoo.com>
2014-02-24 00:09:18 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -230,7 +230,7 @@ function injectCSS() {
 		\n\
 		.btnTool{height:34px;padding:0px;border-radius:2px;}\n\
 		\n\
-		.btnToolContainer{height:32px;padding:6px 12px;margin:0px;font-size:14px;font-weight:normal;line-height:1.428571429;text-align:center;vertical-align:middle;cursor:pointer;border-radius:inherit;border-top:1px solid transparent;white-space:nowrap;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none;user-select:none;}\n\
+		.btnToolContainer{height:34px;padding:6px 12px;margin:0px;font-size:14px;font-weight:normal;line-height:1.428571429;text-align:center;vertical-align:middle;cursor:pointer;border-radius:inherit;border-top:1px solid transparent;white-space:nowrap;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none;user-select:none;}\n\
 		.btnToolContainer:focus{outline:thin dotted #333;outline:5px auto -webkit-focus-ring-color;outline-offset:-2px;}\n\
 		.btnToolContainer:hover,.btnTool:focus{color:#333333;text-decoration:none;}\n\
 		.btnToolContainer:active,.btnToolContainer.active{outline:0;background-image:none;-webkit-box-shadow:inset 0 3px 5px rgba(0,0,0,0.125);box-shadow:inset 0 3px 5px rgba(0,0,0,0.125);}\n\
