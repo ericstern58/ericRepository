787f38a7eeba44cfa35482aacbdb590da9eb6fff
ericstern58 <ericstern58@yahoo.com>
2014-02-23 18:25:43 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -222,9 +222,6 @@ function injectCSS() {
 		.btnTool-group>.btnTool-group:last-child>.btnTool:first-child{border-bottom-left-radius:0;border-top-left-radius:0;}\n\
 		.btnTool-group .dropdown-toggle:active,.btnTool-group.open .dropdown-toggle{outline:0;}\n\
 		\n\
-		.btnTool-group>.btnTool+.dropdown-toggle{padding-left:8px;padding-right:8px;}\n\
-		.btnTool-group.open .dropdown-toggle {-webkit-box-shadow:inset 0 3px 5px rgba(0,0,0,0.125);box-shadow:inset 0 3px 5px rgba(0,0,0,0.125);}\n\
-		\n\
 		.btnTool{display:inline-block;padding:6px 12px;margin-bottom:0;font-size:14px;font-weight:normal;line-height:1.428571429;text-align:center;vertical-align:middle;cursor:pointer;border:1px solid transparent;border-radius:4px;white-space:nowrap;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none;user-select:none;}\n\
 		.btnTool:focus{outline:thin dotted #333;outline:5px auto -webkit-focus-ring-color;outline-offset:-2px;}\n\
 		.btnTool:hover,.btnTool:focus{color:#333333;text-decoration:none;}\n\
