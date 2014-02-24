1676ac95753d3268326bc54009d1613898c5fa37
ericstern58 <ericstern58@yahoo.com>
2014-02-24 15:35:53 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -138,7 +138,9 @@ function floodFill(e){
 				colorPixelBlend(d,w,x,y+1,fillColor,new RGBColor(d[4*w*(y+1)+4*x],d[4*w*(y+1)+4*x+1],d[4*w*(y+1)+4*x+2]));
 				*/
 			}
-		}/*
+		}
+		var x = 0;
+		var y = 0;
 		while(outerEdgeQueue.length>0) {
 			var point=queue.shift();
 			x=point.x;
@@ -146,7 +148,7 @@ function floodFill(e){
 			if(x>=0 && y>=0 && x<w && y<h && targetColor.equals(new RGBColor(d[4*w*y+4*x],d[4*w*y+4*x+1],d[4*w*y+4*x+2])) ) {
 				colorPixel(d,w,point,fillColor)
 			}
-		}*/
+		}
 	}
 }
 
