2ac83904e6a217e9da6a7e7c90380dfc6db839ce
ericstern58 <ericstern58@yahoo.com>
2014-02-23 22:59:44 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -179,10 +179,16 @@ function createToolButton(name){
 	radio.setAttribute("name","drawToolRadio");
 	button.appendChild(radio);
 	
-	// Create container div
+	
+	//Create container div
+	var container = document.createElement('div');
+	container.id = 'drawToolContainer-' + name;
+	button.appendChild(container);
+	
+	// Create icon div
 	var icon = document.createElement('div');
-	icon.id = 'drawToolContainer-' + name;
-	button.appendChild(icon);
+	icon.id = 'drawToolIcon-' + name;
+	container.appendChild(icon);
 	
 	return button;
 }
@@ -206,11 +212,11 @@ function injectCSS() {
 	var sheet = document.createElement('style');
 	sheet.innerHTML = "\n\
 		\n\
-		#drawToolContainer-fill{width:12px;height:12px;margin:7px -6px 1px 10px;background:black;border-bottom-right-radius:2px;border-bottom-left-radius:2px;-webkit-transform:rotate(-40deg);-moz-transform:rotate(-40deg);-ms-transform:rotate(-40deg);-o-transform:rotate(-40deg);transform:rotate(-40deg);-webkit-transform-origin:0 100%;-moz-transform-origin:0 100%;-ms-transform-origin:0 100%;-o-transform-origin:0 100%;transform-origin:0 100%;}\n\
-		#drawToolContainer-fill:before{content:'';border-bottom:5px solid black;border-left:8px solid transparent;border-right:8px solid transparent;display:block;position:absolute;top:-6px;left:-6px;}\n\
-		#drawToolContainer-line{width:5px;height:15px;margin:3px 5px 2px 5px;background:black;-webkit-transform:skew(-50deg);-moz-transform:skew(-50deg);-o-transform:skew(-50deg);transform:skew(-50deg);}\n\
-		#drawToolContainer-poly{width:20px;margin:11px -3px 1px -3px;border-width:8px 4px 0;border-style:solid;border-color:black transparent;}\n\
-		#drawToolContainer-poly:before{content:'';display:block;margin:-17px 0px 0px -4px;border-width:0 10px 9px;border-style:solid;border-color:transparent transparent black;}\n\
+		#drawToolIcon-fill{width:12px;height:12px;margin:7px -6px 1px 10px;background:black;border-bottom-right-radius:2px;border-bottom-left-radius:2px;-webkit-transform:rotate(-40deg);-moz-transform:rotate(-40deg);-ms-transform:rotate(-40deg);-o-transform:rotate(-40deg);transform:rotate(-40deg);-webkit-transform-origin:0 100%;-moz-transform-origin:0 100%;-ms-transform-origin:0 100%;-o-transform-origin:0 100%;transform-origin:0 100%;}\n\
+		#drawToolIcon-fill:before{content:'';border-bottom:5px solid black;border-left:8px solid transparent;border-right:8px solid transparent;display:block;position:absolute;top:-6px;left:-6px;}\n\
+		#drawToolIcon-line{width:5px;height:15px;margin:3px 5px 2px 5px;background:black;-webkit-transform:skew(-50deg);-moz-transform:skew(-50deg);-o-transform:skew(-50deg);transform:skew(-50deg);}\n\
+		#drawToolIcon-poly{width:20px;margin:11px -3px 1px -3px;border-width:8px 4px 0;border-style:solid;border-color:black transparent;}\n\
+		#drawToolIcon-poly:before{content:'';display:block;margin:-17px 0px 0px -4px;border-width:0 10px 9px;border-style:solid;border-color:transparent transparent black;}\n\
 		\n\
 		.btnTool-group,{position:relative;display:inline-block;vertical-align:middle;}\n\
 		.btnTool-group>.btnTool{position:relative;float:left;display:inline-block;}\n\
