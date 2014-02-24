515472842910df3f9f3754bc4b3034765d2cda10
ericstern58 <ericstern58@yahoo.com>
2014-02-23 23:54:40 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -158,7 +158,7 @@ function createToolButtonWithLabel(name, label){
 	var button = createToolButton(name);
 	
 	//Place element inside it( text )
-	button.innerHTML = label;
+	button.getElementsByTagName('div')[0] = label;
 	
 	return button;
 }
