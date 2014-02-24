b3b4116ca4dab6e4e86ca0e61dd3d0a8404633a6
ericstern58 <ericstern58@yahoo.com>
2014-02-24 15:02:57 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -123,12 +123,12 @@ function floodFill(e){
 				queue.push(new Point(x,y+1));
 			} else if(x>=0 && y>=0 && x<w && y<h){
 				//colorPixel(d,w,point,fillColor)
-				
+				/*
 				colorPixelBlend(d,w,x-1,y,fillColor,new RGBColor(d[4*w*y+4*(x-1)],d[4*w*y+4*(x-1)+1],d[4*w*y+4*(x-1)+2]));
 				colorPixelBlend(d,w,x+1,y,fillColor,new RGBColor(d[4*w*y+4*(x+1)],d[4*w*y+4*(x+1)+1],d[4*w*y+4*(x+1)+2]));
 				colorPixelBlend(d,w,x,y-1,fillColor,new RGBColor(d[4*w*(y-1)+4*x],d[4*w*(y-1)+4*x+1],d[4*w*(y-1)+4*x+2]));
 				colorPixelBlend(d,w,x,y+1,fillColor,new RGBColor(d[4*w*(y+1)+4*x],d[4*w*(y+1)+4*x+1],d[4*w*(y+1)+4*x+2]));
-				
+				*/
 			}
 		}
 	}
