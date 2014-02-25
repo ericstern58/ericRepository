889415d13974ce3536475df6dfad9550b768ba52
ericstern58 <ericstern58@yahoo.com>
2014-02-24 22:31:06 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -141,16 +141,10 @@ function floodFill(e){
 			} else if(point.isWithinBounds && !(fillColor.equals(getColorFromPoint(point)))){
 				// If inside this block, current pixel is an edge pixel
 				colorPixel(point,fillColor);
-				// Create some blending between outeredge colors to antialias
-				colorPixelBlend(point,fillColor,getColorFromCoords(x-1,y));
-				colorPixelBlend(point,fillColor,getColorFromCoords(x+1,y));
-				colorPixelBlend(point,fillColor,getColorFromCoords(x,y-1));
-				colorPixelBlend(point,fillColor,getColorFromCoords(x,y+1));
-				
-				colorPixelBlend(point,fillColor,getColorFromCoords(x-1,y-1));
-				colorPixelBlend(point,fillColor,getColorFromCoords(x-1,y+1));
-				colorPixelBlend(point,fillColor,getColorFromCoords(x+1,y-1));
-				colorPixelBlend(point,fillColor,getColorFromCoords(x+1,y+1));
+				colorPixelBlend2(point,fillColor,getColorFromCoords(x-1,y));
+				colorPixelBlend2(point,fillColor,getColorFromCoords(x+1,y));
+				colorPixelBlend2(point,fillColor,getColorFromCoords(x,y-1));
+				colorPixelBlend2(point,fillColor,getColorFromCoords(x,y+1));
 			}
 		}
 	}
@@ -166,6 +160,12 @@ function floodFill(e){
 	}
 	//Colors a pixel with a blend of 2 colors (helpful for assimilating anti-aliasing)
 	function colorPixelBlend(point,color1,color2){
+		var r=Math.ceil(0.5*color1.r /*+ 0.5*color2.r*/);
+		var g=Math.ceil(0.5*color1.g /*+ 0.5*color2.g*/);
+		var b=Math.ceil(0.5*color1.b /*+ 0.5*color2.b*/);
+		colorPixel(point,new RGBColor(r,g,b));
+	}
+	function colorPixelBlend2(point,color1,color2){
 		var r=Math.ceil((color1.r+color2.r)/2);
 		var g=Math.ceil((color1.g+color2.g)/2);
 		var b=Math.ceil((color1.b+color2.b)/2);
