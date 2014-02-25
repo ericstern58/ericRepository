749631ba3e5f6f2cd55003407fbc2fcdb75c667f
ericstern58 <ericstern58@yahoo.com>
2014-02-24 21:59:38 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -132,19 +132,20 @@ function floodFill(e){
 			x=point.x;
 			y=point.y;
 			if( point.isWithinBounds && targetColor.equals(getColorFromPoint(point)) ) {
-				colorPixel(point,fillColor)
+				//colorPixel(point,fillColor)
+				colorPixelBlend(point,fillColor, targetColor);
 				queue.push(new Point(x-1,y));
 				queue.push(new Point(x+1,y));
 				queue.push(new Point(x,y-1));
 				queue.push(new Point(x,y+1));
 			} else if(x>=0 && y>=0 && x<w && y<h){
 				//colorPixel(d,w,point,fillColor)
-				
+				/*
 				colorPixelBlend(x-1,y,fillColor,new RGBColor(d[4*w*y+4*(x-1)],d[4*w*y+4*(x-1)+1],d[4*w*y+4*(x-1)+2]));
 				colorPixelBlend(x+1,y,fillColor,new RGBColor(d[4*w*y+4*(x+1)],d[4*w*y+4*(x+1)+1],d[4*w*y+4*(x+1)+2]));
 				colorPixelBlend(x,y-1,fillColor,new RGBColor(d[4*w*(y-1)+4*x],d[4*w*(y-1)+4*x+1],d[4*w*(y-1)+4*x+2]));
 				colorPixelBlend(x,y+1,fillColor,new RGBColor(d[4*w*(y+1)+4*x],d[4*w*(y+1)+4*x+1],d[4*w*(y+1)+4*x+2]));
-				
+				*/
 			}
 		}
 	}
