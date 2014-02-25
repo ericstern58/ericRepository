39c6dca5b16cff8a584f9ea4e0ccf4e0027fc70c
ericstern58 <ericstern58@yahoo.com>
2014-02-24 23:28:52 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -143,8 +143,8 @@ function floodFill(e){
 				 //Experimental
 				colorPixelBlend(point,fillColor,getColorFromCoords(x-1,y));
 				colorPixelBlend(point,fillColor,getColorFromCoords(x+1,y));
-				//colorPixelBlend(point,fillColor,getColorFromCoords(x,y-1));
-				//colorPixelBlend(point,fillColor,getColorFromCoords(x,y+1));
+				colorPixelBlend(point,fillColor,getColorFromCoords(x,y-1));
+				colorPixelBlend(point,fillColor,getColorFromCoords(x,y+1));
 			}
 		}
 	}
@@ -160,9 +160,9 @@ function floodFill(e){
 	}
 	// [Experimental] Colors a pixel with a blend of 2 colors (helpful for assimilating anti-aliasing)
 	function colorPixelBlend(point,color1,color2){
-		var r=Math.ceil((color1.r+color2.r)/2);
-		var g=Math.ceil((color1.g+color2.g)/2);
-		var b=Math.ceil((color1.b+color2.b)/2);
+		var r=Math.ceil((color1.r*0.3+color2.r*0.7));
+		var g=Math.ceil((color1.g*0.3+color2.g*0.7));
+		var b=Math.ceil((color1.b*0.3+color2.b*0.7));
 		colorPixel(point,new RGBColor(r,g,b));
 	}
 	function getColorFromPoint(point){
