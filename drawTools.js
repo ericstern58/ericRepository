0b784b90a7417704865ba9a6a42decc6c9ca4e9a
ericstern58 <ericstern58@yahoo.com>
2014-02-25 02:11:57 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -153,18 +153,6 @@ function floodFill(e){
 
 			colorPixel(point,fillColor);
 			
-			var point2 = new Point(x-1,y);
-			if(point2.isWithinBounds)
-				colorPixelBlend(point2,fillColor,getColorFromCoords(x-1,y));
-			point2 = new Point(x+1,y);
-			if(point2.isWithinBounds)
-				colorPixelBlend(point2,fillColor,getColorFromCoords(x+1,y));
-			point2 = new Point(x,y-1));
-			if(point2.isWithinBounds)
-				colorPixelBlend(point2,fillColor,getColorFromCoords(x,y-1));
-			point2 = new Point(x,y+1);
-			if(point2.isWithinBounds)
-				colorPixelBlend(point2,fillColor,getColorFromCoords(x,y+1));
 		}
 	}
 	
