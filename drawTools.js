a7fbad672436333cb32c6bdb6fc9b09e5e0e3ad7
ericstern58 <ericstern58@yahoo.com>
2014-02-26 04:15:20 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -66,7 +66,7 @@ function outputDebug(outputString){
 // Setup Mousedown Listener
 //drawApp.canvas.removeEventListener('pointerdown', drawApp.onCanvasMouseDown(),!1);
 
-drawApp.canvas.addEventListener("mousedown",function(e){
+//drawApp.canvas.addEventListener("mousedown",function(e){
 //drawApp.canvas.on('mousedown', function(e){
 	if(currentToolType == toolType.BRUSH)
 		return;//drawApp.onCanvasMouseDown(e);	// default behaviors
@@ -92,8 +92,8 @@ drawApp.canvas.addEventListener("mousedown",function(e){
 	} else{	//Else it is unknown, do nothing
 		alert('toolType not identified');
 	}
-//});
-}, false)();
+});
+//}, false)();
 // Setup Mousemove Listener
 document.onmousemove = function(e) {
  	//outputDebug( (e.pageX-canvasOffset.left) + ', ' + (e.pageY-canvasOffset.top));
