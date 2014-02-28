aa1bf651877950d19bc28198d9a75b282b00f093
ericstern58 <ericstern58@yahoo.com>
2014-02-28 15:06:01 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -113,11 +113,7 @@ createDrawToolsElements();	// Create Draw Tools Elements and Interface
 // Setup Mousedown Listener
 DACanvas.off('mousedown');
 DACanvas.on('mousedown', function(e){
-	if($('#drawTools-options').css('opacity') == 1){
-		painting = !1;
-		toggleOptions();
-		return;
-	} else if(currentToolType === toolType.BRUSH)
+	if(currentToolType === toolType.BRUSH)
 		return;
 	toolInUse = true;
 	DTUpdateCanvasStateVariables();
@@ -185,7 +181,11 @@ $(document).on('mousemove', function(e){
 // Setup Mouseup Listener
 $(document).off('mouseup');
 $(document).on('mouseup', function(e){
-	if(currentToolType === toolType.BRUSH)
+	if($('#drawTools-options').css('opacity') == 1){
+		restoreCanvas();
+		toggleOptions();
+		return;
+	} else if(currentToolType === toolType.BRUSH)
 		return;
 	else if(!toolInUse)	// If no tool is in use, ignore event
 		return;
