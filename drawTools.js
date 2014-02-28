02b1bb55a7df1ccfd79cef1ca9f91aec8ca3d8e2
ericstern58 <ericstern58@yahoo.com>
2014-02-27 16:49:41 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -353,7 +353,7 @@ function setupCSS()
 		#drawTools-btn-exit .drawTools-btn-container:hover,#drawTools-btn-options .drawTools-btn:focus{background-color:#b90c0c;border-bottom:1px solid #980909;}\n\
 		#drawTools-btn-exit .drawTools-btn-container:active{background-color:#a50000;border-bottom:1px solid #a50000;}\n\
 		\n\
-		#drawTools-options{margin-top:-46px;background:#252525;border-bottom:1px solid #171717;width:300px;height:150px;padding:8px;position:absolute;border-radius:2px;opacity:0;-webkit-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);-moz-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);}\n\
+		#drawTools-options{margin-top:-46px;background:#252525;border-bottom:1px solid #171717;width:300px;height:150px;padding:8px;position:absolute;border-radius:2px;opacity:1;-webkit-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);-moz-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);}\n\
 		\n\
 		";
 	document.body.appendChild(DTSheet);
