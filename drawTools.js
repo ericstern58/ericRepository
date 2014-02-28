e8aefd35af0e152034bc445f97ac42af0f62146b
ericstern58 <ericstern58@yahoo.com>
2014-02-27 17:39:05 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -313,7 +313,7 @@ function setupCSS()
 	// Calculate variables used in css
 	try{
 	var optionsMarginTop = $("#drawTools").offset().top - canvasOffset().top + canvasHeight;
-	catch(err){alert(err)};
+	}catch(err){alert(err)};
 	//outputDebug(optionsMarginTop);
 	
 	var DTSheet = document.createElement('style');
