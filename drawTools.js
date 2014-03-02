c4a0cdbe16c3d81073e6aa2ff64f65a80a7f2ff6
ericstern58 <ericstern58@yahoo.com>
2014-03-01 23:31:15 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -702,30 +702,6 @@ function pointsToArray(points) {
 
 
 
-
-function getControlPoints(x0,y0,x1,y1,x2,y2,t){
-	//  x0,y0,x1,y1 are the coordinates of the end (knot) pts of this segment
-	//  x2,y2 is the next knot -- not connected here but needed to calculate p2
-	//  p1 is the control point calculated here, from x1 back toward x0.
-	//  p2 is the next control point, calculated here and returned to become the 
-	//  next segment's p1.
-	//  t is the 'tension' which controls how far the control points spread.
-	
-	//  Scaling factors: distances from this knot to the previous and following knots.
-	var d01=Math.sqrt(Math.pow(x1-x0,2)+Math.pow(y1-y0,2));
-	var d12=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-	
-	var fa=t*d01/(d01+d12);
-	var fb=t-fa;
-  
-	var p1x=x1+fa*(x0-x2);
-	var p1y=y1+fa*(y0-y2);
-	
-	var p2x=x1-fb*(x0-x2);
-	var p2y=y1-fb*(y0-y2);  
-	
-	return [p1x,p1y,p2x,p2y]
-}
 function drawSpline(ctx,pts,t,closed,editMode){
 	var cp=[];   // array of control points, as x0,y0,x1,y1,...
 	var n=pts.length;
@@ -734,14 +710,8 @@ function drawSpline(ctx,pts,t,closed,editMode){
 	// First check for some base cases
 	if(n == 0) {
 		return;
-	} else if(n == 2) {
-		// Draw Point
-		ctx.beginPath();
-		ctx.moveTo( pts[0], pts[1] );
-		ctx.lineTo( pts[0]+1, pts[1]);
-		ctx.stroke();
 	} else if(n == 4) {
-		// Draw Point
+		// Draw Line
 		ctx.beginPath();
 		ctx.moveTo( pts[0], pts[1] );
 		ctx.lineTo( pts[2], pts[3]);
@@ -757,7 +727,31 @@ function drawSpline(ctx,pts,t,closed,editMode){
 	} 
 	
 	for(var i=0, m = (n-4+(4*isClosedSpline));i<m;i+=2){
-		cp=cp.concat(getControlPoints(pts[i],pts[i+1],pts[i+2],pts[i+3],pts[i+4],pts[i+5],t));
+		// Calculate Control Points
+		//  x0,y0,x1,y1 are the coordinates of the end (knot) pts of this segment
+		//  x2,y2 is the next knot -- not connected here but needed to calculate p2
+		//  p1 is the control point calculated here, from x1 back toward x0.
+		//  p2 is the next control point, calculated here and returned to become the 
+		//  next segment's p1.
+		//  t is the 'tension' which controls how far the control points spread.
+		//  Scaling factors: distances from this knot to the previous and following knots.
+		var x0=pts[i], y0=pts[i+1], x1=pts[i+2], y1=pts[i+3], x2=pts[i+4], y2=pts[i+5];
+		
+		var d01=Math.sqrt(Math.pow(x1-x0,2)+Math.pow(y1-y0,2));
+		var d12=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		
+		var fa=t*d01/(d01+d12);
+		var fb=t-fa;
+	  
+		var p1x=x1+fa*(x0-x2);
+		var p1y=y1+fa*(y0-y2);
+		
+		var p2x=x1-fb*(x0-x2);
+		var p2y=y1-fb*(y0-y2);  
+		
+		return [p1x,p1y,p2x,p2y]
+		// Then add them to array
+		cp=cp.concat([p1x,p1y,p2x,p2y]);
 	}
 	
 	
