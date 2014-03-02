5738a6b2cd850c6a8b6fcd53fa9e1712735fac37
ericstern58 <ericstern58@yahoo.com>
2014-03-02 00:43:59 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -765,6 +765,8 @@ function drawSpline(ctx,pts,t,closed,editMode){
 		ctx.bezierCurveTo(cp[2*i-2],cp[2*i-1],cp[2*i],cp[2*i+1],pts[i+2],pts[i+3]);
 	
 	if(isClosedSpline) {
+		// Draw last curve which closes spline
+		ctx.bezierCurveTo(cp[2*n-2],cp[2*n-1],cp[2*n],cp[2*n+1],pts[n+2],pts[n+3]);
 		ctx.moveTo(pts[0],pts[1]);
 		ctx.closePath();
 		ctx.fillStyle = '#8ED6FF';
