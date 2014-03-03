da7638d8bf5e42e3e061d54e99a0b15cebcd2647
ericstern58 <ericstern58@yahoo.com>
2014-03-02 18:52:43 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -51,25 +51,26 @@ context.putImageData = CanvasRenderingContext2D.prototype.putImageData;
  /*--------------------- Custom Objects/Structures/enums -----------------------*/
 /*-----------------------------------------------------------------------------*/
 var DTOptionsClass = function (idNameString) {
+	this.idName = idNameString;
 	
 	this.shapeFillEnabled = false;
 	this.shapeFillColor;
 };
 DTOptionsClass.prototype.getOffset = function () {
-	return $("#drawTools-options").offset();
+	return $(this.idName).offset();
 };
 DTOptionsClass.prototype.toggleMenu = function () {
 	var h = 150;	// Height of the options div
-	var opacity = $('#drawTools-options').css('opacity');
+	var opacity = $(this.idName).css('opacity');
 	
 	if(opacity == 0) {
-		$("#drawTools-options").stop(true, true).animate({
+		$(this.idName).stop(true, true).animate({
 			height: (h + "px"),
 			marginTop: ("-=" + h + "px"),
 			opacity: "1"
 		},300, "swing");
 	} else if(opacity == 1) {
-		$("#drawTools-options").stop(true, true).animate({
+		$(this.idName).stop(true, true).animate({
 			height: "0px",
 			marginTop: ("+=" + h + "px"),
 			opacity: "0"
@@ -77,10 +78,10 @@ DTOptionsClass.prototype.toggleMenu = function () {
 	}
 };
 DTOptionsClass.prototype.isWithinBounds = function (x, y) {
-	var x2 = x - $("#drawTools-options").offset().top;
-	var y2 = y - $("#drawTools-options").offset().left;
-	var width = $('#drawTools-options').width();
-	var height = $('#drawTools-options').height();
+	var x2 = x - $(this.idName).offset().top;
+	var y2 = y - $(this.idName).offset().left;
+	var width = $(this.idName).width();
+	var height = $(this.idName).height();
 	//outputDebug("[x:" + x2 + ", y:" + y2 + "] [width:" + width + ", height:" + height + "]");
 	return (x2>=0 && y2>=0 && x2<width && y2<height);
 };
