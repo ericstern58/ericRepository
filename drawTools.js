efb30b786e42fe276a86584a115378396bdf1830
ericstern58 <ericstern58@yahoo.com>
2014-03-03 18:21:57 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -661,6 +661,7 @@ function setupCSS()
 		#drawTools-btn-exit .drawTools-btn-container:active{background-color:#a50000;border-bottom:1px solid #a50000;}\n\
 		\n\
 		#drawTools-options{margin-top:"+optionsMarginTop+"px;background:#252525;border-bottom:1px solid #171717;width:300px;height:0px;position:absolute;border-radius:2px 2px 0px 0px;opacity:0;overflow:hidden;-webkit-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);-moz-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);}\n\
+		#drawTools-options-content{border:1px solid #ffffff;position:absolute;top:8px;left:8px;right:8px;bottom:8px;}\n\
 		\n\
 		";
 	document.body.appendChild(DTSheet);
