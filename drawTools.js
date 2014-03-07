0f212c81646a8a86657e82743a727414e7681941
ericstern58 <ericstern58@yahoo.com>
2014-03-06 22:47:46 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -437,7 +437,8 @@ function floodFill(ctx,e){
 	// Note: target color must be different to execute function f
 	// If something is already colored the fill color, nothing needs to be done
 	if(!targetColor.equals(fillColor))
-		paint(5,w-5,50,fillColor);
+		colorPixel(new Point(5,5),fillColor);
+		//paint(5,w-5,50,fillColor);
 		//f(e.offsetX,e.offsetY);
 
 	ctx.putImageData(p,0,0);
