9074cbc517131a7e49b80750b4a395c6f64e2691
ericstern58 <ericstern58@yahoo.com>
2014-03-07 16:42:36 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -475,10 +475,10 @@ function f(xSeed,ySeed){
 	}
 	var resetArray = function(resetRightOne){
 		if(resetRightOne) {
-			edgeArrayRight.push(currentEdgeArrayRight);
+			edgeArrayRight[edgeArrayRight.length] = (currentEdgeArrayRight);
 			currentEdgeArrayRight=[];
 		} else {
-			edgeArrayLeft.push(currentEdgeArrayLeft);
+			edgeArrayLeft[edgeArrayLeft.length] = (currentEdgeArrayLeft);
 			currentEdgeArrayLeft=[];
 		}
 	}
@@ -528,10 +528,11 @@ function f(xSeed,ySeed){
 				// Two if statements to know when to add a new seed
 				if(topFillable && topRightUnfillable) {
 					stack.push([i,y+direction,direction]);
-					
+					//resetArray(true);
 				}
 				if(bottomFillable && bottomRightUnfillable) {
 					stack.push([i,y-direction,-direction]);
+					//resetArray(false);
 				}
 			}
 			addToEdgeArray(false,i,y); // Push right boundary pixel
