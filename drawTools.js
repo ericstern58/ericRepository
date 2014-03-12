18174297125fdff6a113f30d98bef541c5fa6a9b
ericstern58 <ericstern58@yahoo.com>
2014-03-12 01:38:58 -0700

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -816,36 +816,25 @@ function setupCSS()
   /*-----------------------------------------------------------------------------*/
  /*---------------------- Elements Creation/Manipulation -----------------------*/
 /*-----------------------------------------------------------------------------*/
-function setupCssAndHtml(){
-	createDrawToolsContainer();     // Create Draw Tools Container
-	setupCSS();                     // Setup necessary CSS for DrawTools
-	modifyExistingElements();       // Make Necessary Modifications to Existing Elements
-	createDrawToolsElements();      // Create Draw Tools Elements and Interface
-}
-
-function modifyExistingElements() 
-{
-	/*	// TODO:Figure this out. This doesn't work for some reason, so i hardcoded it.
-	for(var j=0;j<cleanTools.dcBrushes.length;j++)
-		document.getElementById(cleanTools.dcBrushes[j].id).parentNode.onclick = function(){selectBrushAUX(cleanTools.dcBrushes[j].size);};
-	*/
+function setupCssAndHtml()
+	/*---- 1. Create Draw Tools Container - DIV in which DrawTools will be placed in ----*/
+	var drawToolsDiv = document.createElement('div');
+	drawToolsDiv.id = cleanTools.id;
+	cleanTools.dcToolbar.appendChild(drawToolsDiv);
+	
+	/*---- 2. Setup necessary CSS for DrawTools ----*/
+	setupCSS();                     
+	
+	/*---- 3. Make Necessary Modifications to Existing Elements ----*/
 	document.getElementById(cleanTools.dcBrushes[0].id).parentNode.onclick = function(){cleanTools.html.buttonHandlers.brushClick(cleanTools.dcBrushes[0].size);};
 	document.getElementById(cleanTools.dcBrushes[1].id).parentNode.onclick = function(){cleanTools.html.buttonHandlers.brushClick(cleanTools.dcBrushes[1].size);};
 	document.getElementById(cleanTools.dcBrushes[2].id).parentNode.onclick = function(){cleanTools.html.buttonHandlers.brushClick(cleanTools.dcBrushes[2].size);};
 	document.getElementById(cleanTools.dcBrushes[3].id).parentNode.onclick = function(){cleanTools.html.buttonHandlers.brushClick(cleanTools.dcBrushes[3].size);};
-}
+	/*	// TODO:Figure this out. This doesn't work for some reason, so i hardcoded it.
+	for(var j=0;j<cleanTools.dcBrushes.length;j++)
+		document.getElementById(cleanTools.dcBrushes[j].id).parentNode.onclick = function(){selectBrushAUX(cleanTools.dcBrushes[j].size);};*/
 
-function createDrawToolsContainer(){
-	//Create DIV in which DrawTools will be placed in
-	var drawToolsDiv = document.createElement('div');
-	drawToolsDiv.id = cleanTools.id;
-	//drawToolsDiv.className = 'drawTools-btn-group';
-	cleanTools.dcToolbar.appendChild(drawToolsDiv);
-}
-
-function createDrawToolsElements() 
-{
-	var drawToolsDiv = document.getElementById(cleanTools.id);
+	/*---- 4. Create Draw Tools Elements and Interface ----*/
 	
 	// Create Tool Buttons
 	createToolButton(cleanTools.tools.toolType.FILL,"fill");
@@ -860,13 +849,13 @@ function createDrawToolsElements()
 	var optionsButton = createUtilityButton("options");
 	optionsButton.onclick = function(){options.toggleMenu();};
 	
-	createOptionsMenu(drawToolsDiv);
+	createOptionsMenu(document.getElementById(cleanTools.id));
 	
 	// Exitbutton to remove DrawTools
 	var exitButton = createUtilityButton("exit");
 	exitButton.onclick = function(){DTDestroy();};
-	
 }
+
 
 //Creates Tool Buttons (wit a label)
 function createToolButtonWithLabel(type, name, label)
