4bae4aeaa658c8323c7d3943871d1f2a0d56ec37
ericstern58 <ericstern58@yahoo.com>
2014-03-11 20:12:21 -0700

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -974,12 +974,12 @@ function createOptionsMenu(drawToolsDiv)
 	document.getElementById('drawTools-options-palette').innerHTML = optionsPaletteHtml;
 	
 	var colorButtons = document.getElementsByName("drawTools-options-palette-radio");
-	alert("Number of colors: " + colorButtons.length + "   and number of paletecolors:" + colorElements.length);
-	/*
+	//alert("Number of colors: " + colorButtons.length + "   and number of paletecolors:" + colorElements.length);
+	
 	for(var i=0;i<colorElements.length;i++) {
 		var color = colorElements[i].getAttribute("data-color");
-		colorButtons[i+2]parent.onclick = function(){cleanTools.html.buttonHandlers.setOptionsColor(color);};
-	}*/
+		colorButtons[i+2].parentNode.onclick = function(){cleanTools.html.buttonHandlers.setOptionsColor(color);};
+	}
 }
 function setLineToolsOpen() {
 	options.lineToolsShouldClose = document.getElementById('drawTools-options-checkbox-lineToolsOpen').checked;
