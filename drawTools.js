9bd3835a49785b977aedd2b24bebaec2dea05da1
ericstern58 <ericstern58@yahoo.com>
2014-08-22 23:49:53 -0700

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -8,6 +8,9 @@ var DRAWCEPTION_TOOLBAR = document.getElementById('redo-button').parentNode.pare
 var DRAWCEPTION_BRUSHES = 
 	[{id: 'brush-2', size: 2},{id: 'brush-5', size: 5},{id: 'brush-12', size: 12},{id: 'brush-35', size: 35}];
 
+var trashVar = 23;
+trashVar = trashVar +234;
+
   /*-----------------------------------------------------------------------------*/
  /*--------------------- Custom Objects/Structures/enums -----------------------*/
 /*-----------------------------------------------------------------------------*/				 
