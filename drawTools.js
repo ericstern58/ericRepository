9a13ce18177a768ef3884cde56eb61687a2969c3
ericstern58 <ericstern58@yahoo.com>
2014-08-24 16:12:51 -0700

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -573,6 +573,9 @@ cleanTools.html.init['setupCSS'] = function()
 		#drawTools-btn-icon-options1{margin:10px 6px 10px 6px;position:relative;width:24px;height:9px;border:3px solid #c2c2c2;background:transparent;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;}\n\
 		#drawTools-btn-icon-options1:before{margin:0 0 0 -5px;position:absolute;top:0;left:0;content:'';content:'';width:15px;height:11px;border-left:3px solid #c2c2c2;border-bottom:3px solid #c2c2c2;background:transparent;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;}\n\
 		#drawTools-btn-icon-options1:after{margin:8px 0 0 8px;position:absolute;top:0;left:0;content:'';width:4px;height:14px;background:#c2c2c2;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;}\n\
+		#drawTools-btn-icon-fillpalette{margin:4px 8px;position:relative;width:24px;height:6px;background:linear-gradient(to right,#c2c2c2,#c2c2c2 6px,transparent 6px,transparent 9px,#c2c2c2 9px,#c2c2c2 15px,transparent 15px,transparent 18px,#c2c2c2 18px,#c2c2c2 24px);}
+		#drawTools-btn-icon-fillpalette:before{margin:0;position:absolute;top:9px;left:0;content:'';width:24px;height:6px;background:linear-gradient(to right,#c2c2c2,#c2c2c2 6px,transparent 6px,transparent 9px,#c2c2c2 9px,#c2c2c2 15px,transparent 15px,transparent 18px,#c2c2c2 18px,#c2c2c2 24px);}
+		#drawTools-btn-icon-fillpalette:after{margin:0;position:absolute;top:18px;left:0;content:'';width:24px;height:6px;background:linear-gradient(to right,#c2c2c2,#c2c2c2 6px,transparent 6px,transparent 9px,#c2c2c2 9px,#c2c2c2 15px,transparent 15px,transparent 18px,#c2c2c2 18px,#c2c2c2 24px);}
 		\n\
 		#drawTools{position:relative;display:inline-block;vertical-align:middle;}\n\
 		#drawTools>.drawTools-btn{position:relative;float:left;display:inline-block;}\n\
@@ -597,6 +600,11 @@ cleanTools.html.init['setupCSS'] = function()
 		#drawTools-btn-loop .drawTools-btn-container:active{background-color:#252525;border-bottom:1px solid #252525;}\n\
 		#drawTools-btn-loop input[type='checkbox']{position:absolute;top:0;left:0;opacity:0;}\n\
 		#drawTools-btn-loop input[type='checkbox']:checked ~ #drawTools-btn-icon-loop {border:3px dotted #c2c2c2;background:#252525;-webkit-box-shadow:none;-moz-box-shadow:none;box-shadow:none;}\n\
+		\n\
+		#drawTools-btn-fillpalette .drawTools-btn-container{background:#252525;border-bottom:1px solid #171717;border-right:1px solid #111111;}\n\
+		#drawTools-btn-fillpalette .drawTools-btn-container:focus{outline:thin dotted #fff;}\n\
+		#drawTools-btn-fillpalette .drawTools-btn-container:hover,#drawTools-btn-fillpalette .drawTools-btn:focus{background-color:#2e2e2e;border-bottom:1px solid #222222;}\n\
+		#drawTools-btn-fillpalette .drawTools-btn-container:active{background-color:#252525;border-bottom:1px solid #252525;}\n\
 		\n\
 		#drawTools-btn-download .drawTools-btn-container{background:#252525;border-bottom:1px solid #171717;border-right:1px solid #111111;}\n\
 		#drawTools-btn-download .drawTools-btn-container:focus{outline:thin dotted #fff;}\n\
@@ -1066,6 +1074,9 @@ cleanTools.html.init['setupCssAndHtml'] = function()
 			
 	var optionsButton = cleanTools.html.init.createUtilityButton("options");
 	optionsButton.onclick = function(){cleanTools.options.toggleMenu();};
+	
+	var fillpaletteButton = cleanTools.html.init.createUtilityButton("fillpalette");
+	fillpaletteButton.onclick = function(){cleanTools.options.toggleMenu();};
 	
 	cleanTools.html.init.createOptionsMenu(drawToolsDiv, optionsButton);
 	
