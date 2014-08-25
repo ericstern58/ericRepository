725a526dd859eed4efa5fa8cd14d6f5e976c2e49
ericstern58 <ericstern58@yahoo.com>
2014-08-24 17:19:43 -0700

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -632,7 +632,7 @@ cleanTools.html.init['setupCSS'] = function()
 		#drawTools-menu-palette1 div:focus{outline:thin dotted #333;outline:5px auto -webkit-focus-ring-color;}\n\
 		#drawTools-menu-palette1 div:hover,#drawTools-menu-palette1 div:focus,#drawTools-menu-palette1 div:active{border:2px solid red;}\n\
 		\n\
-		#drawTools-menu-palette{width:240px;height:100%;position:relative;margin:0;border:red;}\n\
+		#drawTools-menu-palette{width:240px;height:100%;position:relative;margin:0;background:red;}\n\
 		#drawTools-menu-palette label{width:40px;height:40px;float:left;overflow:hidden;display:inline-block;margin:0;padding=0;}\n\
 		#drawTools-menu-palette input{display:none;visibility:hidden;margin:0px;padding:0px;}\n\
 		#drawTools-menu-palette input:checked + div{border:2px solid #c2c2c2;}\n\
