41e52ce1aa0c372f39800f74ecbfd31b913e773d
ericstern58 <ericstern58@yahoo.com>
2014-09-01 17:11:57 -0700

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -167,7 +167,7 @@ cleanTools["html"] = {
 		if(opacity == 0) {
 			$(menuID).stop(true, true).animate({
 				height: (h + "px"),
-				marginTop: ("-=" + h + "px"),
+				//marginTop: ("-=" + h + "px"),
 				opacity: "1"
 			},100, "swing");
 		}
@@ -177,7 +177,7 @@ cleanTools["html"] = {
 		if(opacity == 1) {
 			$(menuID).stop(true, true).animate({
 				height: "0px",
-				marginTop: ("+=" + h + "px"),
+				//marginTop: ("+=" + h + "px"),
 				opacity: "0"
 			},100, "swing");
 		}
@@ -637,7 +637,7 @@ cleanTools.html.init['setupCSS'] = function()
 		#drawTools-btn-exit .drawTools-btn-container:hover,#drawTools-btn-exit .drawTools-btn:focus{background-color:#b90c0c;border-bottom:1px solid #980909;}\n\
 		#drawTools-btn-exit .drawTools-btn-container:active{background-color:#a50000;border-bottom:1px solid #a50000;}\n\
 		\n\
-		#drawTools-menu{position:absolute;bottom:45px;padding:8px;margin:0;background:#252525;border-bottom:1px solid #171717;width:auto;height:175px;border-radius:2px 2px 0px 0px;opacity:1;z-index:99;-webkit-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);-moz-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);}\n\
+		#drawTools-menu{position:absolute;bottom:45px;padding:8px;margin:0;background:#252525;border-bottom:1px solid #171717;width:auto;height:175px;border-radius:2px 2px 0px 0px;opacity:0;z-index:99;-webkit-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);-moz-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);}\n\
 		#drawTools-menu-content{position:relative;}\n\
 		\n\
 		.drawTools-menu-palette-parent{margin-left:-105px !important;}\n\
