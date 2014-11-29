5ee44baa81a30e8175ef14f4768cda8a0dc7baa9
ericstern58 <ericstern58@yahoo.com>
2014-11-28 20:06:48 -0800

    Update drawTools.js

--- drawTools.js
+++ drawTools.js
@@ -637,9 +637,9 @@ cleanTools.html.init['setupCSS'] = function()
 		.drawTools-menu{position:absolute;bottom:45px;padding:8px;margin:0;background:#252525;border-bottom:1px solid #171717;width:auto;height:175px;border-radius:2px 2px 0px 0px;opacity:0;z-index:99;-webkit-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);-moz-box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);box-shadow:0px 0px 5px 0px rgba(0,0,0,0.75);}\n\
 		#drawTools-menu-content{position:relative;}\n\
 		\n\
-		#drawTools-menu-palette-parent{margin-left:-105px !important;border:1px dashed red;}\n\
+		#drawTools-menu-palette-parent{margin-left:-105px !important;}\n\
 		#drawTools-menu-palette{width:240px;height:100%;position:relative;margin:0;border:1px dashed green;}\n\
-		#drawTools-menu-palette label{width:40px;height:40px;float:left;overflow:hidden;display:inline-block;margin:0;padding=0;}\n\
+		#drawTools-menu-palette label{width:40px !important;height:40px !important;overflow:hidden;display:inline-block;margin:0;padding=0;}\n\
 		#drawTools-menu-palette input{display:none;visibility:hidden;margin:0px;padding:0px;}\n\
 		#drawTools-menu-palette input:checked + div{border:2px solid #c2c2c2;}\n\
 		#drawTools-menu-palette div{width:40px;height:40px;border:2px solid #252525;margin=0;padding=0;line-height:2.428571429;}\n\
